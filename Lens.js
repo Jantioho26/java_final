708a1f9ca1260a05b2a02bb20f325b3ed04f3411
Joy Antioho <j.wildfong@yahoo.com>
2026-07-21 07:21:04 -0400

    java_final

---
+++ Lens.js
@@ -0,0 +1,74 @@
+[
+  {
+    "Title": "24-70",
+    "Price": 50,
+    "Rating": 5,
+    "Brand": "Sony"
+  },
+  {
+    "Title": "85",
+    "Price": 75,
+    "Rating": 5,
+    "Brand": "Sony"
+  },
+  {
+    "Title": "50-150",
+    "Price": 45,
+    "Rating": 5,
+    "Brand": "Sony"
+  },
+  {
+    "Title": "16-24",
+    "Price": 100,
+    "Rating": 4,
+    "Brand": "Sony"
+  },
+  {
+    "Title": "15-36",
+    "Price": 35,
+    "Rating": 5,
+    "Brand": "Cannon"
+  },
+  {
+    "Title": "24-105",
+    "Price": 55,
+    "Rating": 5,
+    "Brand": "Cannon"
+  },
+  {
+    "Title": "100",
+    "Price": 100,
+    "Rating": 4,
+    "Brand": "Cannon"
+  },
+  {
+    "Title": "100-300",
+    "Price": 125,
+    "Rating": 5,
+    "Brand": "Cannon"
+  },
+  {
+    "Title": "35",
+    "Price": 50,
+    "Rating": 5,
+    "Brand": "Nikon"
+  },
+  {
+    "Title": "70-200",
+    "Price": 75,
+    "Rating": 5,
+    "Brand": "Nikon"
+  },
+  {
+    "Title": "28-135",
+    "Price": 125,
+    "Rating": 5,
+    "Brand": "Nikon"
+  },
+  {
+    "Title": "85",
+    "Price": 85,
+    "Rating": 5,
+    "Brand": "Nikon"
+  }
+]
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-<script>
+
+
 function executeSearch2() {
   const query = document.getElementById('searchBox2').value;
   alert("Searching for: " + query);
 }
-</script>
+
 
