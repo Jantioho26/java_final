2ddad8b0416aca320f9a32550d0b106a74e0d821
Joy Antioho <j.wildfong@yahoo.com>
2026-07-23 10:52:10 -0400

    java_final

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function openMenu() {
+ navigation.menu__btn += " menu--open"
+}
+
+function closeMenu() {
+  navigation.menu__btn.remove('menu--open')
+}
+
 
 
 fetch("https://6a5e953098d9f02aed79ac73.mockapi.io/JF/lenses")
