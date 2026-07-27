9dbea69d665c766d32744a6d45c398434fead2c8
Joy Antioho <j.wildfong@yahoo.com>
2026-07-27 16:15:48 -0400

    java_final

--- index.js
+++ index.js
@@ -51,91 +51,14 @@
   
 function getLenses() {
   return [
-{
-    "Title": "Sony 24-70",
-    "Price": 50,
-    "Rating": 5,
-    "URL": "Assests/sony24-70.webp",
-    "Brand": "Sony"
-  },
-  {
-    "Title": "Sony 85",
-    "Price": 75,
-    "Rating": 5,
-    "URL": "Assests/sony85.webp",
-    "Brand": "Sony"
-  },
-  {
-    "Title": "Sony 50-150",
-    "Price": 45,
-    "Rating": 5,
-    "URL": "Assests/sony50-150.webp",
-    "Brand": "Sony"
-  },
-  {
-    "Title": "Sony 16-24",
-    "Price": 100,
-    "Rating": 4,
-    "URL": "Assests/shopping.webp",
-    "Brand": "Sony"
-  },
-  {
-    "Title": "Cannon 15-36",
-    "Price": 35,
-    "Rating": 5,
-    "URL": "Assests/cannon15-36.webp",
-    "Brand": "Cannon"
-  },
-  {
-    "Title": "Cannon 24-105",
-    "Price": 55,
-    "Rating": 5,
-    "URL": "Assests/cannon24-105.webp",
-    "Brand": "Cannon"
-  },
-  {
-    "Title": "Cannon 100",
-    "Price": 100,
-    "Rating": 4,
-    "URL": "Assests/Cannon100.jpeg",
-    "Brand": "Cannon"
-  },
-  {
-    "Title": "Cannon100-300",
-    "Price": 125,
-    "Rating": 5,
-    "URL": "Assests/Cannon 100-300.webp",
-    "Brand": "Cannon"
-  },
-  {
-    "Title": "Nikon 35",
-    "Price": 50,
-    "Rating": 5,
-    "URL": "Assests/nikon35mm.jpeg",
-    "Brand": "Nikon"
-  },
-  {
-    "Title": "Nikon 70-200",
-    "Price": 75,
-    "Rating": 5,
-    "URL": "Assests/nikon70-200.webp",
-    "Brand": "Nikon"
-  },
-  {
-    "Title": "Nikon 28-135",
-    "Price": 125,
-    "Rating": 5,
-    "URL": "Assests/Nikon 28-135 .webp",
-    "Brand": "Nikon"
-  },
-  {
-    "Title": "Nikon 85",
-    "Price": 85,
-    "Rating": 5,
-    "URL": "Assests/Nikon 85mm.jpeg",
-    "Brand": "Nikon"
-    },
-   ]}
+    const API_URL =
+  "https://6a5e953098d9f02aed79ac73.mockapi.io/JF/Lenses"
+
+  async function renderLenses(filter, searchValue = "") {
+  const lensesWrapper = document.querySelector(".lens");
+  let lenses = await getLenses();
+
+
 
  const searchInput = document.querySelector("#searchInput");
 
@@ -143,5 +66,15 @@ function getLenses() {
   renderLenses("", event.target.value);
   });
 
-  renderLenses();
 
+  async function getLenses() {
+  const response = await fetch(API_URL);
+
+  if (!response.ok) {
+    throw new Error(`API request failed: ${response.status}`);
+  }
+
+  return await response.json();
+}
+  renderLenses();
+  }
